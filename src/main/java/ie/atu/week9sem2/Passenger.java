e6cb2507654e3129ea99cfb34919447df2a0cc05
DAVIDPRENDERGAST-STU <g00405007@atu.ie>
2025-04-28 16:53:17 +0100

    Constructor Success working

---
+++ src/main/java/ie/atu/week9sem2/Passenger.java
@@ -0,0 +1,38 @@
+package ie.atu.week9sem2;
+
+public class Passenger {
+    private String title;
+    private String firstName;
+    private String lastName;
+
+    public Passenger(String title, String firstName,String lastName)
+    {
+        this.title = title;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
--- src/test/java/TestPassenger.java
+++ src/test/java/TestPassenger.java
@@ -1,11 +1,22 @@
+import ie.atu.week9sem2.Passenger;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestPassenger {
     Passenger myPass;
     @BeforeEach
     void setup()
     {
-        myPass = new Passenger();
+        myPass = new Passenger("Mr", "David", "Prendergast");
+    }
+    @Test
+    void ConstructorSuccess()
+    {
+        assertEquals("Mr", myPass.getTitle());
+        assertEquals("David", myPass.getFirstName());
+        assertEquals("Prendergast", myPass.getLastName());
     }
 
 
