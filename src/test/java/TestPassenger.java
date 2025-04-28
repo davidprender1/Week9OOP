df6fb1ca1af7d374bfacdcb69335795105db6cf6
DAVIDPRENDERGAST-STU <g00405007@atu.ie>
2025-04-28 17:29:50 +0100

    Added ParameterizedTest

--- src/test/java/TestPassenger.java
+++ src/test/java/TestPassenger.java
@@ -1,6 +1,8 @@
 import ie.atu.week9sem2.Passenger;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -20,9 +22,25 @@ public class TestPassenger {
         assertEquals("Prendergast", myPass.getLastName());
     }
     @Test
-    void testTitle()
+    void testTitleSuccess()
+    {
+        assertEquals("Mr", myPass.getTitle());
+        Passenger myPass2  = new Passenger("Ms", "David", "Prendergast");
+        assertEquals("Ms", myPass2.getTitle());
+        Passenger myPass3  = new Passenger("Mrs", "David", "Prendergast");
+        assertEquals("Mrs", myPass3.getTitle());
+    }
+    @ParameterizedTest
+    @ValueSource(strings = {"Mr","Mrs", "Ms"})
+    void testTitleSuccessAlso(String title)
+    {
+        Passenger myPass4 = new Passenger(title,"David","Prendergast");
+    }
+    @Test
+    void testTitleFail()
     {
         assertThrows(IllegalArgumentException.class,() -> {new Passenger("Dr", "David", "Prendergast");});
+        assertThrows(IllegalArgumentException.class,() -> {new Passenger("", "David", "Prendergast");});
     }
 
 
