adeb410da1481e5a64d7e7f87dd655baa76d78e3
DAVIDPRENDERGAST-STU <g00405007@atu.ie>
2025-04-28 16:37:45 +0100

    First Commit

---
+++ src/test/java/TestPassenger.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.BeforeEach;
+
+public class TestPassenger {
+    Passenger myPass;
+    @BeforeEach
+    void setup()
+    {
+        myPass = new Passenger();
+    }
+
+
+}
